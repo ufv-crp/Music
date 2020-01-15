6aaf191d4f05654e67912f3f7633f1d353c9c44a
Walter <>
2020-01-14 23:46:15 -0300

    Initial base

---
+++ src/Application.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Application = () => (
+  <>
+    Application
+  </>
+)
+
+export default Application;
---
+++ src/Application.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Application from './Application';
+
+test('renders application', () => {
+  render(<Application />);
+});
