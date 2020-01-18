6d8c117b9bbac0ec930ed0d7d803e8e7a4393ee1
Walter <>
2020-01-18 18:18:09 -0300

    Forgot password modularized

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -26,6 +26,8 @@ import { authenticate } from "../../authentication";
 
 import { AuthenticationContext } from "../../states";
 
+import { request } from "graphql-request";
+
 const Login = () => {
   const [authentication, setAuthentication] = useContext(AuthenticationContext);
 
@@ -33,7 +35,7 @@ const Login = () => {
 
   if (authentication.token) history.replace("/dashboard");
 
-  const [forgotPassword, setForgotPassword] = useState(true);
+  const [forgotPassword, setForgotPassword] = useState(false);
 
   const classes = useStyles();
 
@@ -50,85 +52,62 @@ const Login = () => {
           {forgotPassword ? "Login" : "Recover your account"}
         </Typography>
 
-        <Formik
-          initialValues={{
-            email: "",
-            password: "",
-            rememberMe: false
-          }}
-          validate={values => {
-            const errors = {};
-            if (!values.email) {
-              errors.email = "Required";
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-            ) {
-              errors.email = "Invalid email address";
-            }
-            if (!values.password) {
-              errors.password = "Required";
-            }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            authenticate({ email: values.email, password: values.password })
-              .then(response => {
-                setAuthentication({ ...response.login });
+        {forgotPassword && (
+          <Formik
+            initialValues={{ email: "" }}
+            validate={values => {
+              const errors = {};
 
-                history.replace("/dashboard");
-              })
-              .catch(error => {
+              if (!values.email) {
+                errors.email = "Required";
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+              ) {
+                errors.email = "Invalid email address";
+              }
+
+              return errors;
+            }}
+            onSubmit={async (values, { setSubmitting, resetForm }) => {
+              setSubmitting(false);
+
+              const query = `
+                mutation ResetUserPassword($email: String!) {
+                  resetUserPassword(email: $email)
+                }
+              `;
+
+              try {
+                const response = await request(
+                  process.env.REACT_APP_GRAPHQL_URL,
+                  query,
+                  {
+                    email: values.email
+                  }
+                );
+
+                resetForm();
+              } catch (error) {
                 console.log(error);
-              });
-            
-            setSubmitting(false);
-          }}
-        >
-          {props => (
-            <Form className={classes.form}>
-              <Field
-                name="email"
-                type="email"
-                label="Email"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                component={TextField}
-              />
-
-              <br />
-              {forgotPassword && (
+              }
+            }}
+          >
+            {props => (
+              <Form className={classes.form}>
                 <Field
-                  type="password"
-                  label="Password"
-                  name="password"
+                  name="email"
+                  type="email"
+                  label="Email"
                   variant="outlined"
                   margin="normal"
                   fullWidth
                   component={TextField}
                 />
-              )}
-              {forgotPassword && (
-                <FormControlLabel
-                  control={<Checkbox value="remember" color="primary" />}
-                  label="Remember me"
-                />
-              )}
-              {props.isSubmitting && <LinearProgress />}
-              {forgotPassword && (
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.submit}
-                  disabled={props.isSubmitting}
-                  onClick={props.submitForm}
-                >
-                  Sign In
-                </Button>
-              )}
-              {!forgotPassword && (
+
+                <br />
+
+                {props.isSubmitting && <LinearProgress />}
+
                 <Button
                   type="submit"
                   fullWidth
@@ -140,17 +119,111 @@ const Login = () => {
                 >
                   Send Password
                 </Button>
-              )}
-              <Grid container>
-                <Grid item xs>
-                  <Button onClick={() => setForgotPassword(!forgotPassword)}>
-                    {forgotPassword ? "Forgot Password?" : "Back to Login"}
-                  </Button>
-                </Grid>
-              </Grid>
-            </Form>
-          )}
-        </Formik>
+              </Form>
+            )}
+          </Formik>
+        )}
+
+        {forgotPassword === false && (
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+              rememberMe: false
+            }}
+            validate={values => {
+              const errors = {};
+
+              if (!values.email) {
+                errors.email = "Required";
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+              ) {
+                errors.email = "Invalid email address";
+              }
+
+              if (!values.password) {
+                errors.password = "Required";
+              }
+
+              return errors;
+            }}
+            onSubmit={async (
+              values,
+              { setSubmitting, setStatus, resetForm }
+            ) => {
+              try {
+                const response = await authenticate({
+                  email: values.email,
+                  password: values.password
+                });
+
+                setAuthentication({ ...response.login });
+
+                history.replace("/dashboard");
+
+                resetForm();
+              } catch (error) {
+                console.log(error);
+              }
+
+              setSubmitting(false);
+            }}
+          >
+            {props => (
+              <Form className={classes.form}>
+                <Field
+                  name="email"
+                  type="email"
+                  label="Email"
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                  component={TextField}
+                />
+
+                <br />
+
+                <Field
+                  type="password"
+                  label="Password"
+                  name="password"
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                  component={TextField}
+                />
+
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+
+                {props.isSubmitting && <LinearProgress />}
+
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  disabled={props.isSubmitting}
+                  onClick={props.submitForm}
+                >
+                  Sign In
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        )}
+
+        <Grid container>
+          <Grid item xs>
+            <Button onClick={() => setForgotPassword(!forgotPassword)}>
+              {forgotPassword ? "Back to Login" : "Forgot Password?"}
+            </Button>
+          </Grid>
+        </Grid>
       </div>
     </Container>
   );
