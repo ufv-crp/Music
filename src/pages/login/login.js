66f97465dd689ea210c61ed751d20a84728420bd
Walter <>
2020-01-19 11:40:48 -0300

    Formik forms modularized

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -26,7 +26,157 @@ import { authenticate, resetPassword } from "../../authentication";
 
 import { AuthenticationContext } from "../../states";
 
-const Login = properies => {
+const FormikForgotPassword = ({ classes }) => (
+  <Formik
+    initialValues={{ email: "" }}
+    validate={values => {
+      const errors = {};
+
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
+
+      return errors;
+    }}
+    onSubmit={async (values, { setSubmitting, resetForm }) => {
+      setSubmitting(true);
+
+      try {
+        resetPassword({ email: values.email });
+
+        resetForm();
+      } catch (error) {
+        console.log(error);
+      }
+    }}
+  >
+    {props => (
+      <Form className={classes.form}>
+        <Field
+          name="email"
+          type="email"
+          label="Email"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          component={TextField}
+        />
+
+        <br />
+
+        {props.isSubmitting && <LinearProgress />}
+
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+          disabled={props.isSubmitting}
+          onClick={props.submitForm}
+        >
+          Send Password
+        </Button>
+      </Form>
+    )}
+  </Formik>
+);
+
+const FormikSign = ({ classes, setAuthentication, properties }) => (
+  <Formik
+    initialValues={{
+      email: "",
+      password: "",
+      rememberMe: false
+    }}
+    validate={values => {
+      const errors = {};
+
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
+
+      if (!values.password) {
+        errors.password = "Required";
+      }
+
+      return errors;
+    }}
+    onSubmit={async (values, { setSubmitting, resetForm }) => {
+      setSubmitting(true);
+
+      try {
+        const response = await authenticate({
+          email: values.email,
+          password: values.password
+        });
+
+        setAuthentication({ ...response.login });
+
+        resetForm();
+      } catch (error) {
+        console.log(error);
+      }
+
+      properties.history.push("/dashboard");
+    }}
+  >
+    {props => (
+      <Form className={classes.form}>
+        <Field
+          name="email"
+          type="email"
+          label="Email"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          component={TextField}
+        />
+
+        <br />
+
+        <Field
+          type="password"
+          label="Password"
+          name="password"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          component={TextField}
+        />
+
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+
+        {props.isSubmitting && <LinearProgress />}
+
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+          disabled={props.isSubmitting}
+          onClick={props.submitForm}
+        >
+          Sign In
+        </Button>
+      </Form>
+    )}
+  </Formik>
+);
+
+const Login = properties => {
   const { setAuthentication } = useContext(AuthenticationContext);
 
   const [forgotPassword, setForgotPassword] = useState(false);
@@ -46,155 +196,10 @@ const Login = properies => {
           {forgotPassword ? "Login" : "Recover your account"}
         </Typography>
 
-        {forgotPassword && (
-          <Formik
-            initialValues={{ email: "" }}
-            validate={values => {
-              const errors = {};
+        {forgotPassword && FormikForgotPassword({ classes })}
 
-              if (!values.email) {
-                errors.email = "Required";
-              } else if (
-                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-              ) {
-                errors.email = "Invalid email address";
-              }
-
-              return errors;
-            }}
-            onSubmit={async (values, { setSubmitting, resetForm }) => {
-              setSubmitting(true);
-
-              try {
-                resetPassword({ email: values.email });
-
-                resetForm();
-              } catch (error) {
-                console.log(error);
-              }
-            }}
-          >
-            {props => (
-              <Form className={classes.form}>
-                <Field
-                  name="email"
-                  type="email"
-                  label="Email"
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                  component={TextField}
-                />
-
-                <br />
-
-                {props.isSubmitting && <LinearProgress />}
-
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.submit}
-                  disabled={props.isSubmitting}
-                  onClick={props.submitForm}
-                >
-                  Send Password
-                </Button>
-              </Form>
-            )}
-          </Formik>
-        )}
-
-        {forgotPassword === false && (
-          <Formik
-            initialValues={{
-              email: "",
-              password: "",
-              rememberMe: false
-            }}
-            validate={values => {
-              const errors = {};
-
-              if (!values.email) {
-                errors.email = "Required";
-              } else if (
-                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-              ) {
-                errors.email = "Invalid email address";
-              }
-
-              if (!values.password) {
-                errors.password = "Required";
-              }
-
-              return errors;
-            }}
-            onSubmit={async (values, { setSubmitting, resetForm }) => {
-              setSubmitting(true);
-
-              try {
-                const response = await authenticate({
-                  email: values.email,
-                  password: values.password
-                });
-
-                setAuthentication({ ...response.login });
-
-                resetForm();
-              } catch (error) {
-                console.log(error);
-              }
-
-              properies.history.push("/dashboard");
-            }}
-          >
-            {props => (
-              <Form className={classes.form}>
-                <Field
-                  name="email"
-                  type="email"
-                  label="Email"
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                  component={TextField}
-                />
-
-                <br />
-
-                <Field
-                  type="password"
-                  label="Password"
-                  name="password"
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                  component={TextField}
-                />
-
-                <FormControlLabel
-                  control={<Checkbox value="remember" color="primary" />}
-                  label="Remember me"
-                />
-
-                {props.isSubmitting && <LinearProgress />}
-
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.submit}
-                  disabled={props.isSubmitting}
-                  onClick={props.submitForm}
-                >
-                  Sign In
-                </Button>
-              </Form>
-            )}
-          </Formik>
-        )}
+        {forgotPassword === false &&
+          FormikSign({ classes, setAuthentication, properties })}
 
         <Grid container>
           <Grid item xs>
