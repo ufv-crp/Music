29564a96e2579198f919f31efcfac197a4278776
Walter <>
2020-02-02 21:09:27 -0300

    Remove parameters

--- src/authentication/client.js
+++ src/authentication/client.js
@@ -4,14 +4,16 @@ import { getLocalStorageItem } from "../states";
 
 import { initialStateAuthentication } from "../states";
 
-const createAuthenticatedClient = ({ token = null }) => {
+const createAuthenticatedClient = () => {
   const authentication = getLocalStorageItem({
     key: "authentication",
     initialStage: initialStateAuthentication
   });
 
   return createClient({
-    headers: { Authorization: `Bearer ${token ? token : authentication.token}` }
+    headers: {
+      Authorization: `Bearer ${authentication ? authentication.token : ""}`
+    }
   });
 };
 
