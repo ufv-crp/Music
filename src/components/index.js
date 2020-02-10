be62439edcdea1ddfbbb85a85184b329daf8ddb2
Igor Alves <igorsk89@gmail.com>
2020-02-10 00:25:41 -0300

    Breadcrumbs

--- src/components/index.js
+++ src/components/index.js
@@ -4,8 +4,19 @@ import Sidebar from "./sidebar/sidebar";
 
 import Footer from "./footer/footer";
 
+import RouterBreadcrumbs from "./breadcrumbs/breadcrumbs";
+
 import { AccountDetails, AccountProfile } from "./account";
 
 import { UserAddress, UserContact } from "./user";
 
-export { Header, Sidebar, Footer, AccountDetails, AccountProfile, UserAddress, UserContact };
+export {
+  Header,
+  Sidebar,
+  Footer,
+  AccountDetails,
+  AccountProfile,
+  UserAddress,
+  UserContact,
+  RouterBreadcrumbs
+};
